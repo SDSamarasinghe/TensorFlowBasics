cb254a4701083a7c95b0ed4dccf9ad36d26340a1
lksad <it20605902@my.sliit.lk>
2023-02-06 15:47:54 +0530

    Normal distribution

--- TensorflowBasic.py
+++ TensorflowBasic.py
@@ -2,8 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as te
 
-print(te.__version__)
-
-#tensor
-tensorA = te.constant(8)
-print(tensorA)
+#Normal Distribution
+x = te.random.normal((4,4), mean=0, stddev=1)
+print("Normal Distribution")
+print(x)
