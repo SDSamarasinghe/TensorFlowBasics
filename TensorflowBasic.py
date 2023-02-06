342a768c3479f725590526d90c41cd13444e626d
lksad <it20605902@my.sliit.lk>
2023-02-06 15:50:57 +0530

    Uniform distribution in tensorflow

--- TensorflowBasic.py
+++ TensorflowBasic.py
@@ -6,3 +6,8 @@ import tensorflow as te
 x = te.random.normal((4,4), mean=0, stddev=1)
 print("Normal Distribution")
 print(x)
+
+#uniform distribution
+x = te.random.uniform((1,5), minval=0, maxval=2)
+print("Uniform Distribution")
+print(x)
