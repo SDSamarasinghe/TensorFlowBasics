135b18f1d4733fee1006453a80e2044201629104
lksad <it20605902@my.sliit.lk>
2023-02-11 15:16:58 +0530

    introduce shapes

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,6 +2,14 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as te
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.dataset import mnist
+import keras
+from keras import layers
+from keras.datasets import mnist
+
+(x_train, y_train) , (x_test, y_test) = mnist.load_data()
+
+print(x_train.shape)
+print(y_train.shape)
+
+
+
--- TensorflowBasic.py
+++ TensorflowBasic.py
@@ -1,11 +1,11 @@
-# import os
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-# import tensorflow as te
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as te
 
-# # #Normal Distribution
-# # x = te.random.normal((4,4), mean=0, stddev=1)
-# # print("Normal Distribution")
-# # print(x)
+#Normal Distribution
+x = te.random.normal((4,4), mean=0, stddev=1)
+print("Normal Distribution")
+print(x)
 
 # # #uniform distribution
 # # x = te.random.uniform((1,5), minval=0, maxval=2)
