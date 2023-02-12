a5c8b0b1ea0a32f90cdb2d8d26a561c4a682ec36
lksad <it20605902@my.sliit.lk>
2023-02-12 08:50:57 +0530

    code with newly created model

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -14,5 +14,18 @@ print(y_train.shape)
 x_train = x_train.reshape(-1, 28*28).astype("float32") / 255.0
 x_train = x_test.reshape(-1, 28*28).astype("float32") / 255.0
 
+def createModel():
+    model = keras.Sequential()
+    model.add(keras.Input(shape=28*28))
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dense(10))
+    return model
+
+mymodel = createModel()
+
+print(mymodel.summary())
+
+
 
 
